fa847c499b50b084a76a95d5a1912dc974cbf82f
atighehchi <farid.at@gmail.com>
2025-11-30 17:19:57 +0330

    Update fetch_and_build.js

--- fetch_and_build.js
+++ fetch_and_build.js
@@ -46,7 +46,7 @@ async function fetchHTML() {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
         "(KHTML, like Gecko) Chrome/120.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,* /*;q=0.8",   //// remove the space from "* /*"
       "Accept-Language": "fa-IR,fa;q=0.9,en-US;q=0.5"
     }
   });
